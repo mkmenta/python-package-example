618136b6e6fa332a1413ec2881a607210a95a297
mkmenta <mkmenta95@gmail.com>
2020-08-31 01:18:46 +0200

    Increase version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="helloworld-mkmenta",
-    version="0.0.1",
+    version="0.0.2",
     description="Say hello!",
     long_description=long_description,
     long_description_content_type="text/markdown",
