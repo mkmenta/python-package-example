96ce28506d0634aac04bc77b1722b161c167e7ea
mkmenta <mkmenta95@gmail.com>
2020-08-30 23:48:19 +0200

    Move example to a script

---
+++ examples/say_hello_example.py
@@ -0,0 +1,9 @@
+from helloworld import say_hello
+
+# Generate "Hello world!"
+greeting = say_hello()
+print(greeting)
+
+# Generate "Hello mkmenta!"
+greeting = say_hello("mkmenta")
+print(greeting)
